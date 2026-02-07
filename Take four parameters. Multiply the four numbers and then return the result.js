87eb8d43251be17790d0be5ec168de62285b62d1
sakibulislam75 <sakibulislam1623@gmail.com>
2026-02-07 12:38:13 +0600

    add first problem

---
+++ Take four parameters. Multiply the four numbers and then return the result.js
@@ -0,0 +1,7 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1, num2, num3, num4) {
+    let multi = (num1 * num2 * num3 * num4);
+    return multi;
+}
+console.log(multiply(2, 2, 2, 2));
